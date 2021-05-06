c4a2d97d9ae3c89abe3c0fc1c02bb4f3ea8048ea
Skazmen <skaziuwielkisz69@gmail.com>
2021-05-06 21:29:57 +0200

    added login and registery windows and realtime database (firebase) with users accounts

---
+++ LoginRegisteryApp/app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -0,0 +1,92 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_register.*
+import java.util.*
+
+class RegisterActivity : AppCompatActivity() {
+    companion object {
+        val TAG = "RegisterActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        register_button_register.setOnClickListener {
+            performRegister()
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            Log.d(TAG, "Try to show login activity")
+
+            // launch the login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d(TAG, "Attempting to create user with email: $email")
+
+        // Firebase Authentication to create a user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    return@addOnCompleteListener
+
+                    Toast.makeText(applicationContext, "You are registered", Toast.LENGTH_LONG).show()
+                    saveUserToFirebaseDatabase(it.toString())
+                    Log.d(TAG, "Successfully created user with uid: ${it.result?.user?.uid}")
+
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            .addOnFailureListener{
+                Log.d(TAG, "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, username_edittext_register.text.toString())
+
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d(TAG, "Finally we saved the user to Firebase Database")
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "Failed to set value to database: ${it.message}")
+            }
+    }
+}
+
+class User(val uid: String, val username: String)
