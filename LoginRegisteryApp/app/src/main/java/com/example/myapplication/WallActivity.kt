219bf0724ad995a5d2c24e9f427edf82b8375cc8
Michał Reclik <michal-reclik@wp.pl>
2021-05-17 18:46:09 +0200

    create pot/comm xml

---
+++ LoginRegisteryApp/app/src/main/java/com/example/myapplication/WallActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.ServerConnection.UserRequests
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_wall.*
+import org.json.JSONObject
+
+class WallActivity : AppCompatActivity(), ServerLisener {
+    companion object {
+        const val TAG: String = "WallActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wall)
+
+        //recyclerView adapter
+//        postsList.adapter = PostAdapter(tasks, this)
+        postsList.layoutManager = LinearLayoutManager(this)
+        postsList.setHasFixedSize(false)
+    }
+
+
+
+    override fun onResponseArrived(requestId: Int, error: String?, response: JSONObject?) {
+        finish()
+    }
+}
