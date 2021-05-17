2bfe6615887858d4ab3a7b26ed0b17230039768b
Rivus44 <rivus44@gmail.com>
2021-05-17 18:30:16 +0200

    Added adapters, fixed minor issues

---
+++ LoginRegisteryApp/app/src/main/java/com/example/myapplication/CommentAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class CommentAdapter(private var usernames: List<String>, private var comments: List<String>):
+    RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        val commentUsername: TextView = itemView.findViewById(R.id.textView4)
+        val commentText: TextView = itemView.findViewById(R.id.textView5)
+
+        init{
+            itemView.setOnClickListener { v: View ->
+                val position: Int = adapterPosition
+                Toast.makeText(
+                    itemView.context,
+                    "You clicked on item: ${position + 1}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.sub_post, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return usernames.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.commentUsername.text = usernames[position]
+        holder.commentText.text = comments[position]
+
+    }
+
+
+}
---
+++ LoginRegisteryApp/app/src/main/java/com/example/myapplication/PostAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class PostAdapter(private var titles: List<String>, private var details: List<String>,
+                  private var images: List<Int>, private var comments: RecyclerView):
+                  RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+
+
+   inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+       val postTitle: TextView = itemView.findViewById(R.id.textView)
+       val postDetails: TextView = itemView.findViewById(R.id.textView3)
+       val postImage: ImageView = itemView.findViewById(R.id.imageView)
+       var postComments: RecyclerView = itemView.findViewById(R.id.commentRecycler)
+
+       init{
+           itemView.setOnClickListener { v: View ->
+               val position: Int = adapterPosition
+               Toast.makeText(
+                   itemView.context,
+                   "You clicked on item: ${position + 1}",
+                   Toast.LENGTH_SHORT
+               ).show()
+           }
+
+
+       }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.sub_post, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.postTitle.text = titles[position]
+        holder.postDetails.text = details[position]
+        holder.postImage.setImageResource(images[position])
+        holder.postComments = comments
+    }
+
+
+}
