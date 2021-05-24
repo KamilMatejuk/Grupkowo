dc0c1129937c1b9cab0404110c629877334583e4
Rivus44 <rivus44@gmail.com>
2021-05-24 14:50:28 +0200

    Damian: comments - work in progress Michał: visual changes

--- LoginRegisteryApp/app/src/main/java/com/example/myapplication/Comment.kt
+++ LoginRegisteryApp/app/src/main/java/com/example/myapplication/Comment.kt
@@ -2,4 +2,6 @@ package com.example.myapplication
 
 class Comment {
 
+    // TODO: Add needed lists to a comment to add comments quicker
+
 }
--- LoginRegisteryApp/app/src/main/java/com/example/myapplication/Post.kt
+++ LoginRegisteryApp/app/src/main/java/com/example/myapplication/Post.kt
@@ -2,4 +2,6 @@ package com.example.myapplication
 
 class Post {
 
+    // TODO: Add needed lists to a post to add posts quicker
+
 }
