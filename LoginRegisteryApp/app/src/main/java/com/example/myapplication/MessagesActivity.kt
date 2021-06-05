a5ac8f0b3fe9a038bc5ec5389fad16a371f63ba1
kamil <kamilmatejuk@gmail.com>
2021-06-05 16:42:23 +0200

    Chat basic

--- LoginRegisteryApp/app/src/main/java/com/example/myapplication/MessagesActivity.kt
+++ LoginRegisteryApp/app/src/main/java/com/example/myapplication/MessagesActivity.kt
@@ -1,12 +1,65 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.ServerConnection.ChatRequests
 
-class MessagesActivity: AppCompatActivity() {
+class MessagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messeges)
+
+        // TODO get group id
+
+        // move to wall activity
+        findViewById<RecyclerView>(R.id.wall_button).setOnClickListener {
+            val intent = Intent(this, WallActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // send a new message
+        findViewById<RecyclerView>(R.id.wall_button).setOnClickListener {
+            sendMessage()
+        }
+
+        getMessages()
     }
+
+    private fun getMessages() {
+        ChatRequests.getMessages(applicationContext, 0,
+            functionCorrect = { response ->
+                run {
+                    println(response)
+                }
+            },
+            functionError = {
+                run {
+                    Toast.makeText(this, "Couldn't load messages", Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+
+    private fun sendMessage() {
+        val text = findViewById<TextView>(R.id.new_message_text)
+        if (text.text.trim().isNotEmpty()) {
+            ChatRequests.sendMessage(applicationContext, 0, text.text.trim() as String,
+                functionCorrect = {
+                    run {
+                        text.text = ""
+                    }
+                },
+                functionError = {
+                    run {
+                        Toast.makeText(this, "Couldn't send message", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        }
+    }
+
 }
