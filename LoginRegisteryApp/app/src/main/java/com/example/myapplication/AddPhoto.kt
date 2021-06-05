1902337bab89785fbb7ca1a08bc3e652a25e6fc4
kamil <kamilmatejuk@gmail.com>
2021-06-05 18:53:55 +0200

    Minor fixes

--- LoginRegisteryApp/app/src/main/java/com/example/myapplication/AddPhoto.kt
+++ LoginRegisteryApp/app/src/main/java/com/example/myapplication/AddPhoto.kt
@@ -1,17 +1,15 @@
 package com.example.myapplication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.ServerConnection.*
-import com.example.myapplication.databinding.ActivityAccountBinding
-import com.example.myapplication.databinding.ActivityMainBinding
 import android.net.Uri
+import com.example.myapplication.databinding.AddPhotoBinding
+import kotlinx.android.synthetic.main.add_photo.*
 
 class AddPhoto : AppCompatActivity() {
+    private lateinit var binding: AddPhotoBinding
 
     lateinit var photoUri : Uri
 
@@ -19,10 +17,10 @@ class AddPhoto : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_photo)
         val bundle: Bundle? = intent.extras
-        photoUri = bundle!!.getParcelable<Uri>("imgUri")
-        photo.setImageURI(photoUri)
+        photoUri = bundle!!.getParcelable("imgUri")!!
+        binding.photo.setImageURI(photoUri)
 
-        actionButton.setOnClickListener {
+        binding.actionButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             val description = newDescription.text
             intent.apply {
